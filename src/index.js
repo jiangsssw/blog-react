454ae7e8745089be120b8b2a557df9a15913e5f2
96jiang <hitewang@foxmail.com>
2018-05-30 12:26:59 +0800

    amend this login

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
-import{BrowserRouter} from 'react-router-dom';
+import Login from './page/login'
+import{BrowserRouter,Switch,Route} from 'react-router-dom';
 // Render the main component into the dom
 ReactDOM.render(
                 <BrowserRouter>
-                    <App />
+                  <Switch>
+                      <Route path='/login' component={Login}/>
+                      <App />
+                    </Switch>
                  </BrowserRouter>, document.getElementById('app'));
